7afecf20ce2955e0f019a5e489dadf9180217f34
robertfodor-job <79786361+robertfodor-job@users.noreply.github.com>
2022-02-26 19:08:50 +0100

    test

---
+++ test2/app.js
@@ -0,0 +1,6 @@
+var person = {
+    name: "Maximilian",
+    age: 30,
+    hobbies: ["Swimming", "Driving"]
+};
+console.log(person);
---
+++ test2/app.ts
@@ -0,0 +1,11 @@
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+} = {
+  name: "Maximilian",
+  age: 30,
+  hobbies: ["Swimming", "Driving"],
+};
+
+console.log(person);
